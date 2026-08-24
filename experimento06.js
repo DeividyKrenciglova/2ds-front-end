320e6fb7630cd326e027334dc74ae5b146903021
Deividy Oliveira Krenciglova <deividy.krenciglova@escola.pr.gov.br>
2026-08-24 12:13:07 +0000

    24/08 experimento 06

---
+++ experimento06.js
@@ -0,0 +1,6 @@
+const botaoabrir = document.getElementById("btnabrirmodal")
+const modalcontainer = document.getElementById("modalcontainer")
+
+botaoabrir.addEventListener("click", function(){
+    
+});
