7df6c622a103a78558f4c06f39063a4ed278c367
Deividy Oliveira Krenciglova <deividy.krenciglova@escola.pr.gov.br>
2026-08-27 11:18:12 +0000

    27/08 experimento 06

--- experimento06.js
+++ experimento06.js
@@ -2,5 +2,18 @@ const botaoabrir = document.getElementById("btnabrirmodal")
 const modalcontainer = document.getElementById("modalcontainer")
 
 botaoabrir.addEventListener("click", function(){
-    
-});
+    modalcontainer.innerHTML  = `
+        <div class="modal-fundo">
+            <div class="modal-caixa">
+                <h2>Aviso!</h2>
+                <p> Este modal inteiro foi criado 
+                pelo JavaScript.</p>
+                <button id="btnfechamodal">Fecha</button>
+            </div>
+        </div>`;
+
+        const botaofechar = document.getElementById("btnfechamodal");
+        botaofechar.addEventListener("click", function(){
+            modalcontainer.innerHTML = "";
+        })
+})
